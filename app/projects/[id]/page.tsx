c24823359e1cedca4f01a63a9c6a453bf0b0f7f7
aidrivenprojects <aidrivenprojects@gmail.com>
2026-05-29 12:57:37 +0530

    Add files via upload
    Signed-off-by: aidrivenprojects <aidrivenprojects@gmail.com>

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,5 +1,10 @@
 import { ProjectDetail } from "@/components/projects/ProjectDetail";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <ProjectDetail projectId={params.id} />;
+export default async function Page({
+  params
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  return <ProjectDetail projectId={id} />;
 }
