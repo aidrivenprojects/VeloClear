75a6e5b6ee002c34696e679dba68bc78c4312887
aidrivenprojects <aidrivenprojects@gmail.com>
2026-05-30 16:36:45 +0530

    Add files via upload
    Signed-off-by: aidrivenprojects <aidrivenprojects@gmail.com>

---
+++ app/reports/page.tsx
@@ -0,0 +1,2 @@
+import { ReportsPage } from "@/components/enterprise/EnterprisePages";
+export default function Page(){return <ReportsPage/>}
---
+++ app/roles/page.tsx
@@ -0,0 +1,2 @@
+import { RolesPage } from "@/components/enterprise/EnterprisePages";
+export default function Page(){return <RolesPage/>}
